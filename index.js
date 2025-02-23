2ad6ba02259a15c20891df09b513a0453f665191
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-23 21:07:11 +0200

    Add task* js

--- index.js
+++ index.js
@@ -7,12 +7,29 @@ contactForm.addEventListener("submit", submitHandler);
 function submitHandler(event) {
   event.preventDefault();
   
-  const phoneFirst = document.querySelector('#phone-first');
-  const phoneSecond = document.querySelector('#phone-second');
-  const phoneThird = document.querySelector('#phone-third');
+  const firstName = document.querySelector('#first-name').value;
+  const lastName = document.querySelector('#last-name').value;
+  let fullName = firstName + ' ' + lastName;
 
-  let fullPhone= phoneFirst+phoneSecond+phoneThird
+  const email = document.querySelector('#email').value;
+  
+  const phoneFirst = document.querySelector('#phone-first').value;
+  const phoneSecond = document.querySelector('#phone-second').value;
+  const phoneThird = document.querySelector('#phone-third').value;
+  let fullPhone= phoneFirst+phoneSecond+phoneThird;
 
+  const subject = document.querySelector('#message-subject').value;
+
+  const message = document.querySelector('#message').value.trim().replace(/ +/g, ' ');
+  
+  const form ={
+    name: fullName,
+    email: email,
+    phone: fullPhone,
+    subject: subject,
+    message: message
+  }
+  console.log(form);
 
 }
 
