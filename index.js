8ca249c323199d32c49cc5af3efdea73ba3a9251
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-23 20:10:18 +0200

    Add correct

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-'usr strict'
+"usr strict";
+
+const contactForm = document.querySelector("form");
+
+contactForm.addEventListener("submit", submitHandler);
+
+function submitHandler(event) {
+  event.preventDefault();
+  
+  const phoneFirst = document.querySelector('#phone-first');
+  const phoneSecond = document.querySelector('#phone-second');
+  const phoneThird = document.querySelector('#phone-third');
+
+  let fullPhone= phoneFirst+phoneSecond+phoneThird
+
+
+}
+
+
+
